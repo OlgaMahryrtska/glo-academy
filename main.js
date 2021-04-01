3a8d403ad819d51d3bf150f6dee66e693259e847
Olga Magrytska <olia.magritskaia@gmail.com>
2021-04-01 09:58:50 +0200

    message

---
+++ main.js
@@ -0,0 +1,8 @@
+let money = 20,
+  income = 400,
+  addExpenses = 300,
+  deposit = 800,
+  mission = 5000,
+  period = "5";
+alert("Hello, my little friend!");
+console.log("It is time to get breakfast, my little friend!");
